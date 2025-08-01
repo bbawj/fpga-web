2e8ce41db0f2c3cd5233d15dbbbc826e8538185f
brendan <brendanawjang@gmail.com>
2025-08-01 22:54:07 +0800

    clean up FIFO with formal

--- rtl/fifo.sv
+++ rtl/fifo.sv
@@ -22,31 +22,25 @@ module fifo #(
 
 reg [DATA_WIDTH-1:0] mem [0:DEPTH-1];
 // additional bit here allows to differentiate between full and empty
-reg [ADDR_WIDTH:0] wr_ptr, rd_ptr;
+reg [ADDR_WIDTH:0] wr_ptr ='0, rd_ptr = '0;
 
-// Pointer management
+integer i;
 always @(posedge clk) begin
-    if (rst) begin
-        wr_ptr <= 0;
-        rd_ptr <= 0;
-    end else begin
-        if (wr_en && !full)
-            wr_ptr <= wr_ptr + 1;
-        if (rd_en && !empty)
-            rd_ptr <= rd_ptr + 1;
+  if (rst) begin
+    dout <= '0;
+    for (i = 0; i < DEPTH; i = i + 1) mem[i] <= '0;
+    wr_ptr <= 0;
+    rd_ptr <= 0;
+  end else begin
+    if (wr_en && !full) begin
+      wr_ptr <= wr_ptr + 1;
+      mem[wr_ptr[ADDR_WIDTH-1:0]] <= din;
     end
-end
-
-// Memory write
-always @(posedge clk) begin
-    if (wr_en && !full)
-        mem[wr_ptr[ADDR_WIDTH-1:0]] <= din;
-end
-
-// Memory read
-always @(posedge clk) begin
-    if (rd_en && !empty)
-        dout <= mem[rd_ptr[ADDR_WIDTH-1:0]];
+    if (rd_en && !empty) begin
+      rd_ptr <= rd_ptr + 1;
+      dout <= mem[rd_ptr[ADDR_WIDTH-1:0]];
+    end
+  end
 end
 
 // Status flags
@@ -54,4 +48,17 @@ assign full = (wr_ptr - rd_ptr) == DEPTH;
 assign empty = (wr_ptr == rd_ptr);
 assign count = wr_ptr - rd_ptr;
 
+`ifdef FORMAL
+initial assume(rst);
+initial f_past_valid = 1'b0;
+always @(posedge clk) begin
+  f_past_valid <= 1'b1;
+  // expected ways to use this FIFO
+  assume (~rd_en || (rd_en && ~empty));
+  assume (~wr_en || (wr_en && ~full));
+// the data should remain the same as long as rd_en stays low
+if (f_past_valid && !$past(rd_en)) assert($past(rst) || $stable(dout));
+assert(~(full && empty));
+end
+`endif
 endmodule
