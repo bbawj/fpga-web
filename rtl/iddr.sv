2bbf96be297fae1c83136f6ffd233990f32c384d
brendan <brendanawjang@gmail.com>
2026-06-29 15:19:55 +0800

    use DELAYG before IDDR

--- rtl/iddr.sv
+++ rtl/iddr.sv
@@ -1,13 +1,40 @@
+`default_nettype none
+
 module iddr #(
-  parameter INPUT_WIDTH = 1
-  )(
-  input wire clk,
-  input wire [INPUT_WIDTH - 1 : 0] d,
-  output reg [INPUT_WIDTH - 1: 0 ] q1,
-  output reg [INPUT_WIDTH - 1 : 0] q2
-  );
-  reg [INPUT_WIDTH - 1: 0] d_reg_1 = '0;
-  reg [INPUT_WIDTH - 1: 0] d_reg_2 = '0;
+    parameter INPUT_WIDTH = 1
+) (
+    input wire clk,
+    input wire [INPUT_WIDTH - 1 : 0] d,
+    output wire [INPUT_WIDTH - 1:0] q1,
+    output wire [INPUT_WIDTH - 1 : 0] q2
+);
+`ifdef SYNTHESIS
+  genvar i;
+  generate
+    wire [INPUT_WIDTH-1:0] d_q;
+    for (i = 0; i < INPUT_WIDTH; i = i + 1) begin
+      // obtained from: https://github.com/jfrohnhofen/LiME/blob/5496e6c3390ac5efe244243c2c9bf33784a81c77/src/lime/net/Rgmii.scala#L55
+      // Section 5.2 in CrossLink High-Speed I/O MIPI D-PHY and DDR Interfaces Technical Note
+      // Compensate for clock injection delay
+      DELAYG #(
+          .DEL_MODE("SCLK_ALIGNED"),
+          .DEL_VALUE(80)  // i think that only static value supported
+      ) delay_inst (
+          .A(d[i]),
+          .Z(d_q[i])
+      );
+      IDDRX1F iddr_inst (
+          .D(d_q[i]),
+          .SCLK(clk),
+          .RST(1'b0),
+          .Q0(q1[i]),
+          .Q1(q2[i])
+      );
+    end
+  endgenerate
+`else
+  reg [INPUT_WIDTH - 1:0] d_reg_1 = '0;
+  reg [INPUT_WIDTH - 1:0] d_reg_2 = '0;
   always @(posedge clk) begin
     d_reg_1 <= d;
   end
@@ -15,8 +42,7 @@ module iddr #(
     d_reg_2 <= d;
   end
 
-  always @(posedge clk) begin
-    q1 <= d_reg_1;
-    q2 <= d_reg_2;
-  end
+  assign q1 = d_reg_1;
+  assign q2 = d_reg_2;
+`endif
 endmodule
--- rtl/rgmii_rcv.sv
+++ rtl/rgmii_rcv.sv
@@ -1,32 +1,46 @@
 module rgmii_rcv (
-  input wire rst,
-  input wire mii_rxc,
-  input wire [3:0] mii_rxd,
-  input wire mii_rxctl,
+    input wire rst,
+    input wire mii_rxc,
+    input wire [3:0] mii_rxd,
+    input wire mii_rxctl,
 
-  // output wire rxc,
-  output wire rx_dv,
-  output wire rx_er,
-  output reg [7:0] rxd
+    // output wire rxc,
+    output wire rx_dv,
+    output wire rx_er,
+    output wire [7:0] rxd
 );
 
 `ifdef SPEED_100M
-reg counter = 0;
-// clk_divider _div(.clk_in(mii_rxc), .rst(rst), .clk_out(rxc));
-always @(posedge mii_rxc) begin
-  if (rst) begin
-    rxd <= '0;
+  reg counter = 0;
+  // clk_divider _div(.clk_in(mii_rxc), .rst(rst), .clk_out(rxc));
+  always @(posedge mii_rxc) begin
+    if (rst) begin
+      rxd <= '0;
+    end
+    rxd <= {mii_rxd, rxd[7:4]};
   end
-  rxd <= {mii_rxd, rxd[7:4]};
-end
 `else
-reg [3:0] rxd_1, rxd_2;
-assign rxd = {rxd_2, rxd_1};
+  reg [3:0] rxd_1, rxd_2;
+  assign rxd = {rxd_2, rxd_1};
 
-iddr #(.INPUT_WIDTH(4)) rxd_iddr(.clk(mii_rxc), .d(mii_rxd), .q1(rxd_1), .q2(rxd_2));
-// assign rxc = mii_rxc;
+  iddr #(
+      .INPUT_WIDTH(4)
+  ) rxd_iddr (
+      .clk(mii_rxc),
+      .d  (mii_rxd),
+      .q1 (rxd_1),
+      .q2 (rxd_2)
+  );
+  // assign rxc = mii_rxc;
+  // TODO: deal with rx_er
+  iddr #(
+      .INPUT_WIDTH(1)
+  ) rxctl_iddr (
+      .clk(mii_rxc),
+      .d  (mii_rxctl),
+      .q1 (rx_dv),
+      .q2 (rx_er)
+  );
 `endif
-// TODO: deal with rx_er
-iddr #(.INPUT_WIDTH(1)) rxctl_iddr(.clk(mii_rxc), .d(mii_rxctl), .q1(rx_dv), .q2(rx_er));
 
 endmodule
